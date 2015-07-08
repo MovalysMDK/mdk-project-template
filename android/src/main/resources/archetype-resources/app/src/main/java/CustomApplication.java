3c0d39916dd44c2c2a834e4e6b7a012c5b267c6e
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-07-08 09:02:47 +0000

    update template for mdk-widget

--- android/src/main/resources/archetype-resources/app/src/main/java/CustomApplication.java
+++ android/src/main/resources/archetype-resources/app/src/main/java/CustomApplication.java
@@ -1,11 +1,29 @@
 package ${package};
 
 import com.adeuza.movalysfwk.mobile.mf4android.application.MFAndroidApplication;
+import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetApplication;
+import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetComponentProvider;
+import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetSimpleComponentProvider;
 
-public class CustomApplication extends MFAndroidApplication {
+/**
+ * Custom application.
+ *
+ */
+public class CustomApplication extends MFAndroidApplication implements MDKWidgetApplication {
 
+	/**
+	 * Component provider for mdk widget.
+	 */
+	private MDKWidgetSimpleComponentProvider mdkWidgetComponentProvider ;
+	
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		mdkWidgetComponentProvider = new MDKWidgetSimpleComponentProvider(this);
+	}
+	
+	@Override
+	public MDKWidgetComponentProvider getMDKWidgetComponentProvider() {
+		return mdkWidgetComponentProvider;
 	}
 }
