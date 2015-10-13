d74ec1f9d6bfdbed61854b853ff623172b0e542b
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-10-13 07:41:38 +0000

    fix android template

--- android/src/main/resources/archetype-resources/app/src/main/java/CustomApplication.java
+++ android/src/main/resources/archetype-resources/app/src/main/java/CustomApplication.java
@@ -1,8 +1,10 @@
-package ${package};
+package ${package}.mbandroid;
 
 import com.adeuza.movalysfwk.mobile.mf4android.application.MFAndroidApplication;
 import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetApplication;
+import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetComponentActionHelper;
 import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetComponentProvider;
+import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetSimpleComponentActionHelper;
 import com.soprasteria.movalysmdk.widget.core.provider.MDKWidgetSimpleComponentProvider;
 
 /**
@@ -16,14 +18,25 @@ public class CustomApplication extends MFAndroidApplication implements MDKWidget
 	 */
 	private MDKWidgetSimpleComponentProvider mdkWidgetComponentProvider ;
 	
+	/**
+	 * Component action helper for mdk widget.
+	 */
+	private MDKWidgetSimpleComponentActionHelper mdkWidgetComponentActionHelper;
+	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		mdkWidgetComponentProvider = new MDKWidgetSimpleComponentProvider(this);
+		mdkWidgetComponentActionHelper = new MDKWidgetSimpleComponentActionHelper();
 	}
 	
 	@Override
 	public MDKWidgetComponentProvider getMDKWidgetComponentProvider() {
 		return mdkWidgetComponentProvider;
+	}
+
+	@Override
+	public MDKWidgetComponentActionHelper getMDKWidgetComponentActionHelper() {
+		return mdkWidgetComponentActionHelper;
 	}
 }
