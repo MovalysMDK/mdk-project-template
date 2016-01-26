f6461714f0ef0975b591e67a23f548e241fb0502
El Amrani Benoit <benoit.elamrani@soprasteria.com>
2016-01-26 16:56:26 +0100

    update protractor conf file and package.json to download selenium server

--- html5/src/main/resources/archetype-resources/webapp/protractor.conf.js
+++ html5/src/main/resources/archetype-resources/webapp/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
 
   // The location of the selenium standalone server .jar file.
-  seleniumServerJar: 'D:/Profiles/belamrani/AppData/Roaming/npm/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
   // The port to start the selenium server on, or null if the server should
   // find its own unused port.
   seleniumPort: null,
@@ -19,7 +19,7 @@ exports.config = {
   // find chromedriver. This will be passed to the selenium jar as
   // the system property webdriver.chrome.driver. If null, selenium will
   // attempt to find chromedriver using PATH.
-  chromeDriver: 'D:/Profiles/belamrani/AppData/Roaming/npm/node_modules/protractor/selenium/chromedriver',
+  chromeDriver: './node_modules/protractor/selenium/chromedriver',
   // Additional command line options to pass to selenium. For example,
   // if you need to change the browser timeout, use
   // seleniumArgs: ['-browserTimeout=60'],
