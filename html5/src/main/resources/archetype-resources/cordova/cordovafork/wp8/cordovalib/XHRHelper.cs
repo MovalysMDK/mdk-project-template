72fd0b9e89fffbb573a0d004a58006ef1a8862d2
sbernardin <simon.bernardin@sopra.com>
2016-07-27 17:31:07 +0200

    fix sur windows phone 8 : la classe XHRHelper n'implemente pas les methodes AttachNativeHandlers et detachNativeHandlers de l'interface IBrowserDecorator

--- html5/src/main/resources/archetype-resources/cordova/cordovafork/wp8/cordovalib/XHRHelper.cs
+++ html5/src/main/resources/archetype-resources/cordova/cordovafork/wp8/cordovalib/XHRHelper.cs
@@ -1,4 +1,4 @@
-﻿/*  
+/*  
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
@@ -321,6 +321,16 @@ namespace WPCordovaClassLib.CordovaLib
             }
 
             return false;
+        }
+        
+        public void AttachNativeHandlers()
+        {
+            // nothing todo
+        }
+
+        public void DetachNativeHandlers()
+        {
+            // nothing to do
         }
     }
 }
