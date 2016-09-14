6a4098c5611432e5806465d31c501265b25cc40e
mlaffargue <michael.laffargue@soprasteria.com>
2016-09-14 15:52:29 +0200

    Re-templating to use JSPM

--- ionic2/src/main/resources/archetype-resources/app/app.ts
+++ ionic2/src/main/resources/archetype-resources/app/app.ts
@@ -1,44 +1,25 @@
-import {Component, ViewChild} from '@angular/core';
-import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import 'zone.js';
+import 'reflect-metadata';
+
+import { Component } from '@angular/core';
+import { ionicBootstrap, Platform } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { HomePage } from './pages/home/home';
 
 
 @Component({
-  templateUrl: 'build/app.html'
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
-class MyApp {
-  @ViewChild(Nav) nav: Nav;
+export class MyApp {
+  rootPage: any = HomePage;
 
-  // make HelloIonicPage the root (or first) page
-  rootPage: any = HelloIonicPage;
-  pages: Array<{title: string, component: any}>;
-
-  constructor(
-    public platform: Platform,
-    public menu: MenuController
-  ) {
-    this.initializeApp();
-
-    // set our app's pages
-    this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage }
-    ];
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
+  constructor(public platform: Platform) {
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
-  }
-
-  openPage(page) {
-    // close the menu when clicking a link from the menu
-    this.menu.close();
-    // navigate to the new page if it is not the current page
-    this.nav.setRoot(page.component);
   }
 }
 
--- ionic2/src/main/resources/archetype-resources/gulpfile.js
+++ ionic2/src/main/resources/archetype-resources/gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
+    debug = require('gulp-debug'),
     gulpWatch = require('gulp-watch'),
     del = require('del'),
     runSequence = require('run-sequence'),
-    argv = process.argv;
+    vfs = require('vinyl-fs'),
+    argv = process.argv,
+    sass = require('gulp-sass'),
+    glob = require('glob'),
+    findFolder = require('node-find-folder'),
+    rename = require('gulp-rename'),
+    cmd=require('node-cmd');
 
 
 /**
@@ -11,9 +18,6 @@ var gulp = require('gulp'),
  * tasks before or after the command.
  */
 gulp.task('serve:before', ['watch']);
-gulp.task('emulate:before', ['build']);
-gulp.task('deploy:before', ['build']);
-gulp.task('build:before', ['build']);
 
 // we want to 'watch' when livereloading
 var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
@@ -27,48 +31,85 @@ gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
  * changes, but you are of course welcome (and encouraged) to customize your
  * build however you see fit.
  */
-var buildBrowserify = require('ionic-gulp-browserify-typescript');
-var buildSass = require('ionic-gulp-sass-build');
 var copyHTML = require('ionic-gulp-html-copy');
-var copyFonts = require('ionic-gulp-fonts-copy');
-var copyScripts = require('ionic-gulp-scripts-copy');
 var tslint = require('ionic-gulp-tslint');
+var sassBuild = require('ionic-gulp-sass-build');
 
 var isRelease = argv.indexOf('--release') > -1;
 
+gulp.task('build', ['clean'], function(){
+  runSequence('lint',
+    ['sass', 'html', 'fonts', 'config', 'project-ts', 'local-dependencies', 'dependencies']
+  );
+});
+
 gulp.task('watch', ['clean'], function(done){
-  runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
+  runSequence('lint',
+    ['sass', 'html', 'fonts', 'config', 'project-ts', 'local-dependencies', 'dependencies'],
     function(){
-      gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
-      gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
-      buildBrowserify({ watch: true }).on('end', done);
+      gulpWatch('app/**/*.scss', {read: false},function(){ gulp.start('sass'); });
+      gulpWatch('app/**/*.html', {read: false},function(){ gulp.start('html'); });
+      gulpWatch('app/**/*.ts', {read: false},function(){ gulp.start('project-ts'); });
+      // Problem with underneath library Chokidar that doesn't follow Symlinks under Win32
+      // gulpWatch('jspm_packages/local/**/*.ts', {followSymlinks: true}, function(){ gulp.start('local-dependencies'); });
+      done();
     }
   );
 });
 
-gulp.task('build', ['clean'], function(done){
-  runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
-    function(){
-      buildBrowserify({
-        minify: isRelease,
-        browserifyOptions: {
-          debug: !isRelease
-        },
-        uglifyOptions: {
-          mangle: false
-        }
-      }).on('end', done);
-    }
-  );
-});
+gulp.task('sass', function() {
+  includePaths = [];
+  includePaths = includePaths.concat(new findFolder('ionicons*/dist/scss'));
+  includePaths = includePaths.concat(new findFolder('ionic-angular*/'));
 
-gulp.task('sass', buildSass);
+  return sassBuild({
+    dest: 'www/build/css',
+    sassOptions: {
+      includePaths: includePaths
+    }
+  });
+
+    
+});
 gulp.task('html', copyHTML);
-gulp.task('fonts', copyFonts);
-gulp.task('scripts', copyScripts);
+gulp.task('fonts', function() {
+  return gulp.src('jspm_packages/npm/ionic-angular*/fonts/**/*.+(ttf|woff|woff2)')
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('www/build/fonts/'));
+});
+
 gulp.task('clean', function(){
   return del('www/build');
 });
+
+gulp.task('project-ts', function(){
+  // Copy Project typescript files
+  return gulp.src('app/**/*.ts').pipe(gulp.dest('www/build/app'));
+});
+
+gulp.task('config', function(){
+  // Copy configuration files
+  gulp.src('jspm.*.js').pipe(gulp.dest('www/build'));
+  return gulp.src('tsconfig.json').pipe(gulp.dest('www/build'));
+});
+
+gulp.task('dependencies', function() {  
+   // Copy SystemJS files
+  gulp.src('jspm_packages/*').pipe(gulp.dest('www/build/jspm_packages'));
+
+  // Copy bundled dependencies (See jspm.config.js)
+  gulp.src('bundles/dep.js').pipe(gulp.dest('www/build'));
+  return gulp.src('bundles/dep.js.map').pipe(gulp.dest('www/build'));
+});
+
+gulp.task('local-dependencies', function() {
+  // Create a symlink to local dependencies  
+  vfs.src(['jspm_packages/local/**/*', 'jspm_packages/local/**/*/**/*', '!jspm_packages/local/**/{jspm_packages,jspm_packages/**}'],
+     {followSymlinks: true}).pipe(vfs.dest('www/build/jspm_packages/local/'));
+
+  // Copy MDK dependencies
+  return gulp.src(['jspm_packages/mdk/**/*']).pipe(gulp.dest('www/build/jspm_packages/mdk'));
+});
+
+
 gulp.task('lint', tslint);
